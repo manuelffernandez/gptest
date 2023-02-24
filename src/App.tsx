0479b5a803b3ee833b4aaef2d11a1e9a9e85cdab
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-02-24 19:58:54 -0300

    integrates Home&Navbar into App comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Navbar } from './components';
+import { Home } from './pages';
+
 function App(): JSX.Element {
-  return <div className='App'>Hello world</div>;
+  return (
+    <div className='App'>
+      <Navbar />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
