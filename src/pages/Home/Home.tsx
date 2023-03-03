66a2daf27b761180bf5aeaa5e4d9b5650a4433d4
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-03-03 12:20:04 -0300

    integrates PeopleTable within Home comp

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,17 @@
+import { PEOPLE } from '@/data/people';
+import { useAppDispatch } from '@/hooks';
+import { addPeople } from '@/redux/states';
+import { useEffect } from 'react';
+import PeopleTable from './components/PeopleTable';
+
 const Home = (): JSX.Element => {
-  return <div>Hello world</div>;
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(addPeople(PEOPLE));
+  }, []);
+
+  return <PeopleTable />;
 };
 
 export default Home;
