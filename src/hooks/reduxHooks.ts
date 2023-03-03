e797a4113fe611c87252a01cd0ea0c061af6964e
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-03-03 19:36:24 -0300

    reorganizes imports within reduxHooks.ts file

--- src/hooks/reduxHooks.ts
+++ src/hooks/reduxHooks.ts
@@ -3,9 +3,9 @@
 // more info on:
 // https://redux-toolkit.js.org/tutorials/typescript#define-typed-hooks
 
-import { useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch, RootState } from '@/redux/store';
 import type { TypedUseSelectorHook } from 'react-redux';
-import type { RootState, AppDispatch } from '@/redux/store';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
